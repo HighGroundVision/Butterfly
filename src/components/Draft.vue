2a3ba7b1ea8d2119a5d7ef6982b19fbe439c7edf
Jamie Webster <jamie@densyfy.com>
2018-07-05 16:57:53 -0400

    prototype

--- src/components/Draft.vue
+++ src/components/Draft.vue
@@ -113,9 +113,9 @@
         </div>
       </b-col>
     </b-row>
-    <b-row>
+    <b-row v-if="calculated">
       <b-col>
-        <GChart type="LineChart" :data="chartData" :options="chartOptions" />
+        <GChart type="ScatterChart" :data="chartData" :options="chartOptions" />
       </b-col>
     </b-row>
   </section>
@@ -128,14 +128,12 @@
 import axios from 'axios'
 import draggable from 'vuedraggable'
 import { GChart } from 'vue-google-charts'
+import lsq from 'least-squares' // https://github.com/jprichardson/least-squares
 
 export default {
   name: 'Draft',
   components: { draggable, GChart },
   methods: {
-    calculate () {
-      console.log('calculate & plot')
-    },
     select (item) {
       if (this.radiant_safe.length !== 2) {
         this.radiant_safe.push(item)
@@ -156,7 +154,6 @@ export default {
       this.heroes = this.heroes.filter(i => i.id !== item.id)
     },
     deselect (item) {
-      debugger
       if (this.radiant_safe.includes(item)) {
         this.heroes.push(item)
         this.radiant_safe = this.radiant_safe.filter(i => i.id !== item.id)
@@ -176,6 +173,84 @@ export default {
         this.heroes.push(item)
         this.dire_off = this.dire_off.filter(i => i.id !== item.id)
       }
+    },
+    calculate () {
+      let radaint = []
+
+      for (let i = 0; i < this.radiant_safe.length; i++) {
+        const hero = this.radiant_safe[i]
+        let data = this.data.filter(i => i.hero_id === hero.id && i.lane_role === 1).map(i => Math.round((i.wins / i.games) * 100))
+        radaint.push(data)
+      }
+
+      for (let i = 0; i < this.radiant_mid.length; i++) {
+        const hero = this.radiant_mid[i]
+        let data = this.data.filter(i => i.hero_id === hero.id && i.lane_role === 2).map(i => Math.round((i.wins / i.games) * 100))
+        radaint.push(data)
+      }
+
+      for (let i = 0; i < this.radiant_off.length; i++) {
+        const hero = this.radiant_off[i]
+        let data = this.data.filter(i => i.hero_id === hero.id && i.lane_role === 3).map(i => Math.round((i.wins / i.games) * 100))
+        radaint.push(data)
+      }
+
+      let dire = []
+
+      for (let i = 0; i < this.dire_safe.length; i++) {
+        const hero = this.dire_safe[i]
+        let data = this.data.filter(i => i.hero_id === hero.id && i.lane_role === 1).map(i => Math.round((i.wins / i.games) * 100))
+        dire.push(data)
+      }
+
+      for (let i = 0; i < this.dire_mid.length; i++) {
+        const hero = this.dire_mid[i]
+        let data = this.data.filter(i => i.hero_id === hero.id && i.lane_role === 2).map(i => Math.round((i.wins / i.games) * 100))
+        dire.push(data)
+      }
+
+      for (let i = 0; i < this.dire_off.length; i++) {
+        const hero = this.dire_off[i]
+        let data = this.data.filter(i => i.hero_id === hero.id && i.lane_role === 3).map(i => Math.round((i.wins / i.games) * 100))
+        dire.push(data)
+      }
+
+      let axis = this.chartAxis
+      let time = [15, 30, 45, 60, 75]
+
+      let data = []
+      data.push(axis)
+
+      for (let i = 0; i < time.length; i++) {
+        const t = time[i]
+        const r = radaint.reduce((acc, item) => acc + item[i], 0) / 5
+        const d = dire.reduce((acc, item) => acc + item[i], 0) / 5
+        data.push([t, r, d])
+      }
+
+      let radaintValues = []
+      let direValues = []
+      for (let i = 0; i < time.length; i++) {
+        const item = data[i + 1]
+        const r = item[1]
+        const d = item[2]
+        radaintValues.push(r)
+        direValues.push(d)
+      }
+
+      let radaintTrend = {}
+      let direTrend = {}
+      let radaintFn = lsq(time, radaintValues, radaintTrend)
+      let direFn = lsq(time, direValues, direTrend)
+
+      console.log('Radaint Trend', radaintTrend)
+      console.log('Radaint Fn', radaintFn(0))
+
+      console.log('Dire Trend', direTrend)
+      console.log('Dire Fn', direFn(0))
+
+      this.chartData = data
+      this.calculated = true
     }
   },
   computed: {
@@ -191,6 +266,7 @@ export default {
   data () {
     return {
       'ready': false,
+      'calculated': false,
       'heroes': [],
       'radiant_safe': [],
       'radiant_mid': [],
@@ -199,30 +275,19 @@ export default {
       'dire_mid': [],
       'dire_off': [],
       'data': [],
-      // Array will be automatically processed with visualization.arrayToDataTable function
-      'chartData': [
-        ['Time', 'Radiant', 'Dire'],
-        ['15', 58, 42],
-        ['30', 56, 44],
-        ['45', 55, 45],
-        ['60', 48, 52],
-        ['60+', 42, 58]
-      ],
+      'chartAxis': ['Time', 'Radiant', 'Dire'],
+      'chartData': [],
       'chartOptions': {
-        title: 'Test',
-        curveType: 'function',
-        hAxis: {title: 'Time'},
-        vAxis: {title: 'Win Rate'},
-        colors: ['#28a745', '#dc3545'],
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Win Rate'
+        },
+        colors: ['#AB0D06', '#007329'],
         trendlines: {
-          0: {
-            type: 'linear',
-            color: '#28a745'
-          },
-          1: {
-            type: 'linear',
-            color: '#dc3545'
-          }
+          0: {type: 'linear', color: '#AB0D06', opacity: 0.6},
+          1: {type: 'linear', color: '#007329', opacity: 0.6}
         }
       }
     }
@@ -234,7 +299,7 @@ export default {
     let p2 = axios.get('/static/data/hero_winrate_lane_duration.json').then((reponse) => { return reponse.data })
     Promise.all([p1, p2]).then((values) => {
       const heroes = values[0]
-      const data = values[0]
+      const data = values[1]
 
       for (let i = 0; i < heroes.length; i++) {
         const hero = heroes[i]
